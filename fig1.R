16260a1ee811c6caf2821816fe05f36db09ecd9f
Juraj Bergman <jurajbergman@gmail.com>
2021-04-20 15:10:02 +0200

    Update fig1.R

--- fig1.R
+++ fig1.R
@@ -2,17 +2,18 @@ library(ggplot2)
 library(cowplot)
 
 ## load data
-data <- read.table("/Users/au612643/Desktop/par/rScripts/github/fig1.txt", sep = "\t", header = T)
+data <- read.table("fig1.txt", sep = "\t", header = T)
+dataFilt = subset(data, countTV>0&countTS>0&countGC>999&cMmb>0&cMmb<5)
 
-dataTemp <- data.frame(POS = rep(data$POS, 3),
-                       chr = rep(data$chr, 3),
-                       cMmb = rep(data$cMmb, 3),
-                       freq = c(data$freqTSnoncpg,data$freqTScpg,data$freqTVnoncpg),
-                       count  = c(data$countTSnoncpg,data$countTScpg,data$countTVnoncpg),
-                       mType = c(rep("TS (CpG-)", dim(data)[1]), rep("TS (CpG+)", dim(data)[1]), rep("TV (CpG-)", dim(data)[1])))
+dataTemp <- data.frame(POS = rep(dataFilt$POS, 3),
+                       chr = rep(dataFilt$chr, 3),
+                       cMmb = rep(dataFilt$cMmb, 3),
+                       freq = c(dataFilt$freqTSnoncpg, dataFilt$freqTScpg, dataFilt$freqTVnoncpg),
+                       count  = c(dataFilt$countTSnoncpg, dataFilt$countTScpg, dataFilt$countTVnoncpg),
+                       mType = c(rep("TS (CpG-)", dim(dataFilt)[1]), rep("TS (CpG+)", dim(dataFilt)[1]), rep("TV (CpG-)", dim(dataFilt)[1])))
 
 ## plots
-f1a <- ggplot(data, aes(x=log2(cMmb), y=log2(freqGC)))  + geom_point(size=0.005, shape=21, alpha=0.5) + 
+f1a <- ggplot(dataFilt, aes(x=log2(cMmb), y=log2(freqGC)))  + geom_point(size=0.005, shape=21, alpha=0.5) + 
   geom_smooth(method="lm", lty =2, lwd=0.7,col="#e41a1c") +theme_bw() +
   xlab(expression(italic(log[2])(cM/Mb))) + ylab(expression(italic(log[2])(italic(f[GC]))))
 
@@ -38,4 +39,4 @@ legf1d <- get_legend(f1d)
 f1d <- f1d + theme(legend.position = "none")
 
 tt <- plot_grid(f1a,f1b,legf1b,f1c,f1d,legf1d, ncol = 3, align="bt", rel_widths = c(2,2,0.8), labels = c("A", "B", "", "C", "D", ""))
-ggsave("/Users/au612643/Desktop/par/rScripts/github/fig1.pdf", tt, width=18.5, height=12, units = "cm")
+ggsave("fig1.pdf", tt, width=18.5, height=12, units = "cm")
