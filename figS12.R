522dc4d9bda4eade73ace48ea29991882756ebe0
Juraj Bergman <jurajbergman@gmail.com>
2021-04-26 16:00:29 +0200

    Add files via upload

---
+++ figS12.R
@@ -0,0 +1,50 @@
+library(ggplot2)
+library(cowplot)
+
+## load data
+data <- read.table("/Users/au612643/Desktop/par/rScripts/github/figS12.txt", header=T, sep = "\t")
+
+#### Supplementary figure 12
+fS12a <- ggplot(subset(data, pop == "PPA"), aes(x=log2(cMmb), y=log2(freq),colour = mType,))  + geom_point(size=0.005,alpha=0.2) + 
+  geom_smooth(method="lm", lty =2, lwd=0.7) +theme_bw() + theme(legend.title = element_blank())+
+  xlab(expression(italic(log[2])(cM/Mb))) + ylab(expression(italic(log[2])(italic(f[GC])))) +
+  scale_color_manual(values = c("#377eb8", "#e41a1c", "#4daf4a"))
+
+legfS12a <- get_legend(fS12a)
+fS12a <- fS12a + theme(legend.position = "none")
+
+fS12b <- ggplot(subset(data, pop == "PPA"), aes(x=log2(cMmb), y=log2(count),colour = mType,))  + geom_point(size=0.005,alpha=0.2) + 
+  geom_smooth(method="lm", lty =2, lwd=0.7) +theme_bw() + theme(legend.position = "none")+
+  xlab(expression(italic(log[2])(cM/Mb))) + ylab(expression(italic(log[2])(Count))) +
+  scale_color_manual(values = c("#377eb8", "#e41a1c", "#4daf4a"))
+
+fS12c <- ggplot(subset(data, pop == "GGG"), aes(x=log2(cMmb), y=log2(freq),colour = mType,))  + geom_point(size=0.005,alpha=0.2) + 
+  geom_smooth(method="lm", lty =2, lwd=0.7) +theme_bw() + theme(legend.title = element_blank())+
+  xlab(expression(italic(log[2])(cM/Mb))) + ylab(expression(italic(log[2])(italic(f[GC])))) +
+  scale_color_manual(values = c("#377eb8", "#e41a1c", "#4daf4a"))
+
+legfS12c <- get_legend(fS12c)
+fS12c <-fS12c + theme(legend.position = "none")
+
+fS12d <- ggplot(subset(data, pop == "GGG"), aes(x=log2(cMmb), y=log2(count),colour = mType,))  + geom_point(size=0.005,alpha=0.2) + 
+  geom_smooth(method="lm", lty =2, lwd=0.7) +theme_bw() + theme(legend.position = "none")+
+  xlab(expression(italic(log[2])(cM/Mb))) + ylab(expression(italic(log[2])(Count))) +
+  scale_color_manual(values = c("#377eb8", "#e41a1c", "#4daf4a"))
+
+fS12e <- ggplot(subset(data, pop == "PAB"), aes(x=log2(cMmb), y=log2(freq),colour = mType,))  + geom_point(size=0.005,alpha=0.2) + 
+  geom_smooth(method="lm", lty =2, lwd=0.7) +theme_bw() + theme(legend.title = element_blank())+
+  xlab(expression(italic(log[2])(cM/Mb))) + ylab(expression(italic(log[2])(italic(f[GC])))) +
+  scale_color_manual(values = c("#377eb8", "#e41a1c", "#4daf4a"))
+
+legfS12e <- get_legend(fS12e)
+fS12e <- fS12e + theme(legend.position = "none")
+
+fS12f <- ggplot(subset(data, pop == "PAB"), aes(x=log2(cMmb), y=log2(count),colour = mType,))  + geom_point(size=0.005,alpha=0.2) + 
+  geom_smooth(method="lm", lty =2, lwd=0.7) +theme_bw() + theme(legend.position = "none")+
+  xlab(expression(italic(log[2])(cM/Mb))) + ylab(expression(italic(log[2])(Count))) +
+  scale_color_manual(values = c("#377eb8", "#e41a1c", "#4daf4a"))
+
+tt <- plot_grid(fS12a, fS12b, legfS12a,
+                fS12c, fS12d, legfS12c,
+                fS12e, fS12f, legfS12e, ncol = 3, align="bt", rel_widths = c(2,2,0.8), labels = c("A", "B", "", "C", "D", "", "E", "F", ""))
+ggsave("/Users/au612643/Desktop/par/rScripts/github/figS12.pdf", tt, width=18.5, height=18, units = "cm")
