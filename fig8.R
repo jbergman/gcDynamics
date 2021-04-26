d910cf3859df133ea3f904fe2c65dfc8e24ef2c7
Juraj Bergman <jurajbergman@gmail.com>
2021-04-26 14:21:17 +0200

    Update fig8.R

--- fig8.R
+++ fig8.R
@@ -2,7 +2,7 @@ library(ggplot2)
 library(cowplot)
 
 ## load data
-data <- read.table("/Users/au612643/Desktop/par/rScripts/github/fig8.txt", header = T, sep = "\t")
+data <- read.table("fig8.txt", header = T, sep = "\t")
 data$pop <- factor(data$pop, levels = c("PPA", "GGG", "PAB"))
 ## Figure 8
 f8a <- ggplot(data, aes(tp, fp, fill= (prop))) +  geom_tile() + facet_grid(pop~mType) + xlab("3' end") + ylab("5' end") + 
@@ -13,4 +13,4 @@ f8b <- ggplot(data, aes(tp, fp, fill= B)) +  geom_tile() + facet_grid(pop~mType)
 
 tt <- plot_grid(f8a, f8b, ncol = 2, labels = c("A", "B"))
 
-ggsave("/Users/au612643/Desktop/par/rScripts/github/fig8.pdf", tt, width=20, height=7.25, units = "cm")
+ggsave("fig8.pdf", tt, width=20, height=7.25, units = "cm")
