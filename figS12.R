e578eeb35d98d7f926f510a0bac2db54c0dec188
Juraj Bergman <jurajbergman@gmail.com>
2021-04-26 16:02:12 +0200

    Update figS12.R

--- figS12.R
+++ figS12.R
@@ -2,9 +2,9 @@ library(ggplot2)
 library(cowplot)
 
 ## load data
-data <- read.table("/Users/au612643/Desktop/par/rScripts/github/figS12.txt", header=T, sep = "\t")
+data <- read.table("figS12.txt", header=T, sep = "\t")
 
-#### Supplementary figure 12
+## Supplementary figure 12
 fS12a <- ggplot(subset(data, pop == "PPA"), aes(x=log2(cMmb), y=log2(freq),colour = mType,))  + geom_point(size=0.005,alpha=0.2) + 
   geom_smooth(method="lm", lty =2, lwd=0.7) +theme_bw() + theme(legend.title = element_blank())+
   xlab(expression(italic(log[2])(cM/Mb))) + ylab(expression(italic(log[2])(italic(f[GC])))) +
@@ -47,4 +47,4 @@ fS12f <- ggplot(subset(data, pop == "PAB"), aes(x=log2(cMmb), y=log2(count),colo
 tt <- plot_grid(fS12a, fS12b, legfS12a,
                 fS12c, fS12d, legfS12c,
                 fS12e, fS12f, legfS12e, ncol = 3, align="bt", rel_widths = c(2,2,0.8), labels = c("A", "B", "", "C", "D", "", "E", "F", ""))
-ggsave("/Users/au612643/Desktop/par/rScripts/github/figS12.pdf", tt, width=18.5, height=18, units = "cm")
+ggsave("figS12.pdf", tt, width=18.5, height=18, units = "cm")
