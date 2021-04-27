d13f2b9a88c57437d0b1e2a2ca0ccffdfdf71e97
Juraj Bergman <jurajbergman@gmail.com>
2021-04-27 10:22:58 +0200

    Update figS10.R

--- figS10.R
+++ figS10.R
@@ -5,9 +5,9 @@ humChr <- c("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr1
 
 for(i in 1:(length(humChr))){
   if(i==1){
-    afYRI <- read.table(paste("/Users/au612643/Desktop/par/thousandGenomes/populations/YRI/counts/chr1.af", sep=""), head=T, sep = "\t")
+    afYRI <- read.table(paste("chr1YRI.af", sep=""), head=T, sep = "\t")
   }else{
-    temp <- read.table(paste("/Users/au612643/Desktop/par/thousandGenomes/populations/YRI/counts/",humChr[i],".af", sep=""), head=T, sep = "\t")
+    temp <- read.table(paste(humChr[i],"YRI.af", sep=""), head=T, sep = "\t")
     afYRI <- rbind(afYRI, temp)
   }
 }
@@ -21,4 +21,4 @@ fS10 <- ggplot() +
       geom_segment(aes(x = median(gcFreqTVcpg) , y = 0, xend = median(gcFreqTVcpg), yend = Inf), lty=2, col="black") +
       xlab(expression(italic(f[GC]))) + ylab("Proportion") + theme_bw()
 
-ggsave("/Users/au612643/Desktop/par/rScripts/github/figS10.pdf", fS10, width=10, height=7, units = "cm")
+ggsave("figS10.pdf", fS10, width=10, height=7, units = "cm")
