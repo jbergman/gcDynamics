4afdc553fa480d83c5065fd444967afc4664719b
Juraj Bergman <jurajbergman@gmail.com>
2021-05-20 16:38:16 +0200

    Update fig7.R

--- fig7.R
+++ fig7.R
@@ -7,12 +7,12 @@ data <- read.table("/Users/au612643/Desktop/par/rScripts/github/fig7.txt", heade
 ## Figure 7
 data$pop <- factor(data$pop, levels = c("PTE","PTS","PTT","PTV","PPA","GGG","PAB","PPY"))
 
-f3a <- ggplot(data, aes(x=mType, y=gamma, color=mType)) + geom_boxplot(outlier.size = 0.5,shape=21) + facet_wrap(.~pop, ncol = 8, nrow = 4) +
+f3a <- ggplot(data, aes(x=mType, y=B, color=mType)) + geom_boxplot(outlier.size = 0.5,shape=21) + facet_wrap(.~pop, ncol = 8, nrow = 4) +
   theme_bw() + theme(legend.position = "none", axis.title.x = element_blank(), axis.text.x = element_text(angle = 90, hjust = 1)) +
   ylab(expression(italic(B)))+ scale_color_manual(values=c("#377eb8", "#e41a1c", "#4daf4a"))+ scale_y_continuous(breaks = c(0.0,0.4,0.8,1.2))+
-  geom_point(aes(x=mType,y=fullGamma), shape=18, size = 4, colour=c(rep("#377eb8",8*23), rep("#e41a1c",8*23), rep("#4daf4a",8*23)))
+  geom_point(aes(x=mType,y=B_full), shape=18, size = 4, colour=c(rep("#377eb8",8*23), rep("#e41a1c",8*23), rep("#4daf4a",8*23)))
 
-f3b <- ggplot(data, aes(x=cMmb, y=gamma, colour = mType)) + geom_point(size=1.25, shape=21, stroke=0, aes(fill=factor(mType))) +
+f3b <- ggplot(data, aes(x=cMmb, y=B, colour = mType)) + geom_point(size=1.25, shape=21, stroke=0, aes(fill=factor(mType))) +
   facet_wrap(.~pop, scales = "free_x", ncol = 8, nrow = 4) + theme_bw() +
   geom_smooth(method="lm",lty =2, lwd=0.7) + theme(legend.position = "none") +xlab("cM/Mb")+ ylab(expression(italic(B)))+
   scale_fill_manual(values = c("#377eb8", "#e41a1c", "#4daf4a")) +  scale_color_manual(values = c("#377eb8", "#e41a1c", "#4daf4a")) +
